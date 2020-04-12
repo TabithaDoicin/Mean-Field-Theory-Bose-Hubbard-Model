cd7f2c1d3f1c395820d1c3fcc9cfe58f7898e1bd
ATDoicin <atdoicin@gmail.com>
2020-04-12 22:35:09 +0100

    Update Bose_Hubbard_sim.py

--- Bose_Hubbard_sim.py
+++ Bose_Hubbard_sim.py
@@ -22,8 +22,8 @@ class SPSystem:   #1d only
         self.L = L
         self.basis = boson_basis_general(self.L, sps = max_p+1)
         self.local_z = [self.z for i in range(self.L)]
-        self.local_z[0] = self.local_z[0] + 1
-        self.local_z[self.L-1] = self.local_z[self.L-1] + 1
+        #self.local_z[0] = self.local_z[0] + 1
+        #self.local_z[self.L-1] = self.local_z[self.L-1] + 1
     
     def construct_h(self, t, mu, psi):
         potential = [[-mu-self.U/2, i] for i in range(self.L)]
@@ -33,11 +33,11 @@ class SPSystem:   #1d only
         extra = [[self.local_z[i]*t*np.abs(psi)**2, i] for i in range(self.L)]
         static = [["n", potential], ["nn", interaction], ["-", hopping_1], ["+", hopping_2], ["I", extra]]
         if self.L>=1:
-            hop = [[-t,i,i+1] for i in range(self.L-1)]
+            hop = [[-t,i,(i+1)] for i in range(self.L-1)]
             static.append(["+-",hop])
             static.append(["-+",hop])
         dynamic = []
-        H = h_construct(static, dynamic, basis = self.basis, dtype = np.complex128, 
+        H = h_construct(static, dynamic, basis = self.basis, dtype = np.complex128,
                      check_symm = False, check_herm = False, check_pcon = False)
         return H.todense()
 
@@ -54,6 +54,7 @@ def simulate(system, xmin, xmax, xsteps, ymin, ymax, ysteps):
     start_time = time.time()
     ti = np.linspace(xmin, xmax, xsteps)
     mui = np.linspace(ymin, ymax, ysteps)
+    global psimat
     psimat = np.zeros((ysteps, xsteps))
     for x in range(xsteps):
         t = ti[x]
@@ -63,13 +64,33 @@ def simulate(system, xmin, xmax, xsteps, ymin, ymax, ysteps):
             psimat[y, x]=np.abs(system.minimize_gs(t, mu))
     elapsed_time = time.time() - start_time
     print('Time elapsed: ' + str(time.strftime("%H:%M:%S", time.gmtime(elapsed_time))))
-    fig, ax = plt.subplots()
-    im = ax.imshow(psimat, origin = 'lower', extent=(xmin,xmax,ymin,ymax), aspect = 'auto')
-    cb = plt.colorbar(im)
-    cb.set_label(r'$\Psi$', size=15)
-    ax.set_xlabel(r'$t/U$', fontsize=15)
-    ax.set_ylabel(r'$\mu/U$', fontsize=15)
-    
-s = SPSystem(U = 1, z = 2, max_p = 3, L = 1) #initialise base system parameters/ z = number of nearest neighbours
-simulate(system = s, xmin = 0, xmax = 0.06, xsteps = 50, 
-         ymin = 0, ymax = 1, ysteps = 50) #calling simulate function, does calculations
+
+
+U = 1
+z = 4
+max_p = 2
+L = 1
+xmin = 0.01
+xmax = 0.06
+xsteps = 20
+ymin = 0
+ymax = 1
+ysteps = 20
+
+s = SPSystem(U, z, max_p, L) #initialise base system parameters/ z = number of nearest neighbours
+simulate(s, xmin, xmax, xsteps, 
+         ymin, ymax, ysteps) #calling simulate function, does calculations
+
+
+fig, ax = plt.subplots()
+im = ax.imshow(psimat, origin = 'lower', extent=(xmin,xmax,ymin,ymax), aspect = 'auto')
+cb = plt.colorbar(im)
+cb.set_label(r'$\Psi$', size=15)
+ax.set_xlabel(r'$t/U$', fontsize=15)
+ax.set_ylabel(r'$\mu/U$', fontsize=15)
+
+fig1,ax1 = plt.subplots()
+ti = np.linspace(xmin,xmax,xsteps)
+ax1.scatter(ti,psimat[int(round(ysteps/2)),:])
+ax1.scatter(ti,psimat[int(round(ysteps/4)),:])
+ax1.scatter(ti,psimat[int(round(3*ysteps/4)),:])
